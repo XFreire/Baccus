45a3e9936423cb0c1e03a443b449eb286e20f4c0
Alexandre Freire García <xandrefreire@gmail.com>
2017-01-03 11:17:55 +0100

    Añadimos un log para aprender a depurar

--- app/src/main/java/com/alexandre/baccus/controller/WineActivity.java
+++ app/src/main/java/com/alexandre/baccus/controller/WineActivity.java
@@ -2,6 +2,7 @@ package com.alexandre.baccus.controller;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ImageView;
 
 import com.alexandre.baccus.R;
@@ -20,6 +21,7 @@ public class WineActivity extends Activity {
 		 *  Desde el controlador modificamos la vista para que muestre
 		 *  otra imagen diferente usando identificadores
 		 */
+        Log.v("Baccus", "Estamos viendo una imagen de un vino");
         mWineImage.setImageResource(R.drawable.vegaval);
     }
 
