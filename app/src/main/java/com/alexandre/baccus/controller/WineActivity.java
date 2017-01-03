74548518a3c622bde00fe46defe1258ab2e279f6
Alexandre Freire García <xandrefreire@gmail.com>
2017-01-03 11:11:46 +0100

    Añadimos un ImageView con una foto de un vino. Cambiamos el nombre del paquete en el Manifest. Enlazamos la vista con el controlador mediante identificadores.

--- app/src/main/java/com/alexandre/baccus/controller/WineActivity.java
+++ app/src/main/java/com/alexandre/baccus/controller/WineActivity.java
@@ -2,14 +2,25 @@ package com.alexandre.baccus.controller;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import com.alexandre.baccus.R;
 
 public class WineActivity extends Activity {
+    private ImageView mWineImage = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wine);
+
+        // Accedemos a la vista desde este controlador
+        mWineImage = (ImageView)findViewById(R.id.wine_image);
+		/*
+		 *  Desde el controlador modificamos la vista para que muestre
+		 *  otra imagen diferente usando identificadores
+		 */
+        mWineImage.setImageResource(R.drawable.vegaval);
     }
+
 }
