e63b61bbce72debcbbbba5413dbb1c2ddbb5e013
Alexandre Freire García <xandrefreire@gmail.com>
2017-01-03 10:57:20 +0100

    Creamos la aplicación

---
+++ app/src/main/java/com/alexandre/baccus/WineActivity.java
@@ -0,0 +1,13 @@
+package com.alexandre.baccus;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class WineActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wine);
+    }
+}
