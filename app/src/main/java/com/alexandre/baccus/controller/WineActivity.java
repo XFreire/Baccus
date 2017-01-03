8822233968bdf3a90018cb29e93bc73121810045
Alexandre Freire García <xandrefreire@gmail.com>
2017-01-03 11:01:01 +0100

    Cambiamos el nombre del paquete y movemos la activity a controller

--- app/src/main/java/com/alexandre/baccus/controller/WineActivity.java
+++ app/src/main/java/com/alexandre/baccus/controller/WineActivity.java
@@ -1,7 +1,9 @@
-package com.alexandre.baccus;
+package com.alexandre.baccus.controller;
 
 import android.app.Activity;
 import android.os.Bundle;
+
+import com.alexandre.baccus.R;
 
 public class WineActivity extends Activity {
 
