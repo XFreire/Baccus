68c1e136173faa2460bea17d61467ad569d3b3f8
Alexandre Freire García <xandrefreire@gmail.com>
2017-01-03 12:49:44 +0100

    Mejoramos la interfaz. Creamos más vistas y la enlazamos con la activity y el modelo

--- app/src/main/java/com/alexandre/baccus/controller/WineActivity.java
+++ app/src/main/java/com/alexandre/baccus/controller/WineActivity.java
@@ -4,7 +4,10 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.RatingBar;
+import android.widget.TextView;
 
 import com.alexandre.baccus.R;
 import com.alexandre.baccus.models.Wine;
@@ -12,8 +15,23 @@ import com.alexandre.baccus.models.Wine;
 import java.util.Arrays;
 
 public class WineActivity extends Activity {
-    private ImageView mWineImage = null; // Vista
+    /**
+     * Vistas
+     */
+    private ImageView mWineImage = null;
+    private TextView mWineNameText = null;
+    private TextView mWineCompanyText = null;
+    private TextView mWineTypeText = null;
+    private TextView mWineOriginText = null;
+    private ViewGroup mWineGrapesContainer = null;
+    private TextView mWineNotesText = null;
+    private RatingBar mWineRatingBar = null;
+
+    /**
+     * Modelo
+     */
     private Wine mWine = null; // Modelo
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,32 +45,26 @@ public class WineActivity extends Activity {
                 "Este vino muestra toda la complejidad y la elegancia de la variedad Mencía. En fase visual luce un color rojo picota muy cubierto con tonalidades violáceas en el menisco. En nariz aparecen recuerdos frutales muy intensos de frutas rojas (frambuesa, cereza) y una potente ciruela negra, así como tonos florales de la gama de las rosas y violetas, vegetales muy elegantes y complementarios, hojarasca verde, tabaco y maderas aromáticas (sándalo) que le brindan un toque ciertamente perfumado.",
                 R.drawable.bembibre,
                 5);
-        // Accedemos a la vista desde este controlador
+        // Asociamos controlador con vistas
         mWineImage = (ImageView)findViewById(R.id.wine_image);
-		/*
-		 *  Desde el controlador modificamos la vista para que muestre
-		 *  otra imagen diferente usando identificadores
-		 */
-        mWineImage.setImageResource(R.drawable.vegaval);
-		// Asociamos la vista con el modelo
+        mWineNameText = (TextView)findViewById(R.id.wine_name);
+        mWineTypeText = (TextView)findViewById(R.id.wine_type);
+        mWineCompanyText = (TextView)findViewById(R.id.wine_company);
+        mWineOriginText = (TextView)findViewById(R.id.wine_origin);
+        mWineGrapesContainer = (ViewGroup)findViewById(R.id.grapes_container);
+        mWineNotesText = (TextView)findViewById(R.id.wine_notes);
+        mWineRatingBar = (RatingBar)findViewById(R.id.wine_rating);
+
+        // Actualizamos la vista con el modelo
         mWineImage.setImageResource(mWine.getPhoto());
+        mWineNameText.setText(mWine.getName());
+        mWineTypeText.setText(mWine.getType());
+        mWineCompanyText.setText(mWine.getWineCompanyName());
+        mWineOriginText.setText(mWine.getOrigin());
+        mWineNotesText.setText(mWine.getNotes());
+        mWineRatingBar.setProgress(mWine.getRating());
 
 
-    }
-
-    public void changeModel(View view){
-        // Cambiamos el modelo
-        this.mWine = new Wine("Vegaval Plata Gran Reserva 2004",
-                "Miguel Calatayud",
-                "Tinto",
-                "Valdepeñas",
-                "http://www.vegaval.com/es/",
-                "Un vino de esmerado proceso de elaboración y larga crianza. Presenta un color rojo cereza con matices a teja y una brillante capa media alta. Nariz compleja, fina y elegante. Es excelentemente estructurado, amplio y muy sabroso. Recomendado para acompañar quesos curados, estofados y todo tipo de carnes rojas y de caza. La temperatura recomendada para servir está entre los 16º C y 18º C.",
-                R.drawable.vegaval,
-                5);
-
-        // Actualizamos la vista
-        mWineImage.setImageResource(mWine.getPhoto());
 
     }
 }
