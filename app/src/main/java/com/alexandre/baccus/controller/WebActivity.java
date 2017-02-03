dfe32bfd4d5ff104c99f2d6155fa9d122440d7df
Alexandre Freire García <xandrefreire@gmail.com>
2017-02-03 17:29:17 +0100

    Creamos UNA CLASE JAVA WebActivity y la hacemos extender de Activity

---
+++ app/src/main/java/com/alexandre/baccus/controller/WebActivity.java
@@ -0,0 +1,17 @@
+package com.alexandre.baccus.controller;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by alexandre on 3/2/17.
+ */
+
+public class WebActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
