695949563ade09eeba5ffae0e22bd4fa0027255c
Alexandre Freire García <xandrefreire@gmail.com>
2017-02-04 17:05:27 +0100

    Modificamos el WebActivity para que herede de FragmentContainerActivity

--- app/src/main/java/com/alexandre/baccus/controller/activity/WebActivity.java
+++ app/src/main/java/com/alexandre/baccus/controller/activity/WebActivity.java
@@ -3,6 +3,7 @@ package com.alexandre.baccus.controller.activity;
 
 import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -15,93 +16,25 @@ import android.webkit.WebViewClient;
 import android.widget.ProgressBar;
 
 import com.alexandre.baccus.R;
+import com.alexandre.baccus.controller.fragment.WebFragment;
 import com.alexandre.baccus.models.Wine;
 
 /**
  * Created by alexandre on 3/2/17.
  */
 
-public class WebActivity extends AppCompatActivity {
-
-    // Atributos estáticos
-    private static final String STATE_URL = "url";
+public class WebActivity extends FragmentContainerActivity {
     public static final String EXTRA_WINE = "WebActivity.EXTRA_WINE";
-    // Modelo
-    private Wine mWine;
-
-    // Vistas
-    private WebView mBrowser = null;
-    private ProgressBar mLoading = null;
-
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_web);
+    protected Fragment createFragment() {
+        Bundle arguments = new Bundle();
+        arguments.putSerializable(WebFragment.ARG_WINE, getIntent().getSerializableExtra(EXTRA_WINE));
 
-        // Creamos el modelo
-        mWine = (Wine) getIntent().getSerializableExtra(EXTRA_WINE);
+        WebFragment fragment = new WebFragment();
 
-        // Asocio vista y controlador
-        mBrowser = (WebView) findViewById(R.id.browser);
-        mLoading = (ProgressBar) findViewById(R.id.loading);
+        fragment.setArguments(arguments);
 
-        // Configuro vistas
-        mBrowser.setWebViewClient(new WebViewClient() {
-
-            @Override
-            public void onPageStarted(WebView view, String url, Bitmap favicon) {
-                super.onPageStarted(view, url, favicon);
-                mLoading.setVisibility(View.VISIBLE);
-            }
-
-            @Override
-            public void onPageFinished(WebView view, String url) {
-                super.onPageFinished(view, url);
-                mLoading.setVisibility(View.GONE);
-            }
-
-            @Override
-            public void onReceivedError(WebView view, WebResourceRequest request, WebResourceError error) {
-                super.onReceivedError(view, request, error);
-                mLoading.setVisibility(View.GONE);
-            }
-        });
-
-        mBrowser.getSettings().setJavaScriptEnabled(true);
-        mBrowser.getSettings().setBuiltInZoomControls(true);
-
-
-        // Cargamos la página. Si no existe un estado previo, cargamos la del modelo
-        if (savedInstanceState == null || !savedInstanceState.containsKey(STATE_URL)) {
-            mBrowser.loadUrl(mWine.getWineCompanyWeb());
-        }
-        else {
-            mBrowser.loadUrl(savedInstanceState.getString(STATE_URL));
-        }
-
-    }
-    @Override
-    protected void onSaveInstanceState(Bundle outState) {
-        super.onSaveInstanceState(outState);
-        outState.putString(STATE_URL, mBrowser.getUrl());
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        super.onCreateOptionsMenu(menu);
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu_web, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        super.onOptionsItemSelected(item);
-        if (item.getItemId() == R.id.menu_reload){
-            mBrowser.reload();
-            return true;
-        }
-        return false;
+        return fragment;
     }
 }
