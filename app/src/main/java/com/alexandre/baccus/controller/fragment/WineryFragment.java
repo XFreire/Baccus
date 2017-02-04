c83beac3bb1b30e8f68cd061dfb3624b5e132130
Alexandre Freire García <xandrefreire@gmail.com>
2017-02-04 22:09:10 +0100

    1. Cambiamos el XML. Escogemos la opción de PagerTitleStrip (luego utilizaremos otra) 2. WineryFragment. 3- Adapter

--- app/src/main/java/com/alexandre/baccus/controller/fragment/WineryFragment.java
+++ app/src/main/java/com/alexandre/baccus/controller/fragment/WineryFragment.java
@@ -4,11 +4,13 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTabHost;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.alexandre.baccus.R;
+import com.alexandre.baccus.controller.adapter.WineryPagerAdapter;
 import com.alexandre.baccus.models.Wine;
 import com.alexandre.baccus.models.Winery;
 
@@ -17,7 +19,7 @@ import com.alexandre.baccus.models.Winery;
  */
 
 public class WineryFragment extends Fragment {
-
+    private ViewPager mPager = null;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -25,21 +27,8 @@ public class WineryFragment extends Fragment {
 
         View root = inflater.inflate(R.layout.fragment_winery, container, false);
 
-        Winery winery = Winery.getInstance();
-        Wine bembibre = winery.getWine(0);
-        Wine vegaval = winery.getWine(1);
-        FragmentTabHost tabHost = (FragmentTabHost) root.findViewById(android.R.id.tabhost);
-        tabHost.setup(getActivity(), getActivity().getSupportFragmentManager(), android.R.id.tabcontent);
-
-        // Añadimos la primera pestaña
-        Bundle arguments = new Bundle();
-        arguments.putSerializable(WineFragment.ARG_WINE, bembibre);
-        tabHost.addTab(tabHost.newTabSpec(bembibre.getName()).setIndicator(bembibre.getName()), WineFragment.class, arguments);
-
-        // Añadimos la segunda pestaña
-        arguments = new Bundle();
-        arguments.putSerializable(WineFragment.ARG_WINE, vegaval);
-        tabHost.addTab(tabHost.newTabSpec(vegaval.getName()).setIndicator(vegaval.getName()), WineFragment.class, arguments);
+        mPager = (ViewPager) root.findViewById(R.id.pager);
+        mPager.setAdapter(new WineryPagerAdapter(getFragmentManager()));
 
         return root;
     }
