c4eb80b23a8052ff966fa31b8408a13c6500550a
Alexandre Freire García <xandrefreire@gmail.com>
2017-02-05 11:11:44 +0100

    Actualizar el ActionBar de cada Fragment con el nombre del vino

--- app/src/main/java/com/alexandre/baccus/controller/fragment/WineryFragment.java
+++ app/src/main/java/com/alexandre/baccus/controller/fragment/WineryFragment.java
@@ -5,6 +5,8 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTabHost;
 import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,8 +20,11 @@ import com.alexandre.baccus.models.Winery;
  * Created by alexandre on 4/2/17.
  */
 
-public class WineryFragment extends Fragment {
+public class WineryFragment extends Fragment implements ViewPager.OnPageChangeListener {
     private ViewPager mPager = null;
+    private ActionBar mActionBar;
+    private Winery mWinery;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -30,6 +35,31 @@ public class WineryFragment extends Fragment {
         mPager = (ViewPager) root.findViewById(R.id.pager);
         mPager.setAdapter(new WineryPagerAdapter(getFragmentManager()));
 
+        mWinery = Winery.getInstance();
+
+        mActionBar = (ActionBar) ((AppCompatActivity) getActivity()).getSupportActionBar();
+        mPager.addOnPageChangeListener(this);
+
+        updateActionBar(0);
         return root;
+    }
+
+    private void updateActionBar(int index) {
+        mActionBar.setTitle(mWinery.getWine(index).getName());
+    }
+
+    @Override
+    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+    }
+
+    @Override
+    public void onPageSelected(int position) {
+        mActionBar.setTitle(mWinery.getWine(position).getName());
+    }
+
+    @Override
+    public void onPageScrollStateChanged(int state) {
+
     }
 }
