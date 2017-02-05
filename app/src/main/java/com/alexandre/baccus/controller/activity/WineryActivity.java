edbba8bf15ee1d73b345d8f0640e18d1cf105252
Alexandre Freire García <xandrefreire@gmail.com>
2017-02-05 18:57:16 +0100

    Ponemos la flechita para volver atrás con la otra opción que tenemos (no usar finish() si no el NavUtils)

--- app/src/main/java/com/alexandre/baccus/controller/activity/WineryActivity.java
+++ app/src/main/java/com/alexandre/baccus/controller/activity/WineryActivity.java
@@ -5,6 +5,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.NavUtils;
+import android.view.MenuItem;
 import android.widget.TabHost;
 
 import com.alexandre.baccus.R;
@@ -14,9 +16,23 @@ import com.alexandre.baccus.models.Wine;
 public class WineryActivity extends FragmentContainerActivity {
     public static final String EXTRA_WINE_INDEX = "WineryActivity.EXTRA_WINE_INDEX"; ;
 
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
 
     @Override
     protected Fragment createFragment() {
         return WineryFragment.newInstance(getIntent().getIntExtra(EXTRA_WINE_INDEX, 0));
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home){
+            NavUtils.navigateUpFromSameTask(this);
+        }
+        return super.onOptionsItemSelected(item);
+
     }
 }
